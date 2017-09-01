c288a2cc8de9255d85560c4215044995fd1b8f14
opure <opure@opuredeiMac.lan>
2017-09-01 23:01:56 +0800

    show picture

--- src/main/java/hello/FileUploadController.java
+++ src/main/java/hello/FileUploadController.java
@@ -1,31 +1,34 @@
 package hello;
 
-import java.io.IOException;
-import java.util.stream.Collectors;
-
+import hello.storage.StorageFileNotFoundException;
+import hello.storage.StorageProperties;
+import hello.storage.StorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import hello.storage.StorageFileNotFoundException;
-import hello.storage.StorageService;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
 
 @Controller
 public class FileUploadController {
 
     private final StorageService storageService;
+
+    @Autowired
+    private  ResourceLoader resourceLoader;
+
+    @Autowired
+    private StorageProperties storageProperties;
 
     @Autowired
     public FileUploadController(StorageService storageService) {
@@ -45,17 +48,18 @@ public class FileUploadController {
 
     @GetMapping("/files/{filename:.+}")
     @ResponseBody
-    public String serveFile(@PathVariable String filename) throws IOException {
+    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
 
-        Resource file = storageService.loadAsResource(filename);
-        System.out.println(file.getURI().toString());
-        System.out.println(file.getURL().toString());
-        return file.getURL().toString();
+        //Resource file = storageService.loadAsResource(filename);
+
+        return ResponseEntity.ok(resourceLoader.getResource("file:" + Paths.get(storageProperties.getLocation(), filename).toString()));
+       // return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+         //       "attachment; filename=\"" + file.getFilename() + "\"").body(file);
     }
 
     @PostMapping("/")
     public String handleFileUpload(@RequestParam("file") MultipartFile file,
-                                   RedirectAttributes redirectAttributes) {
+            RedirectAttributes redirectAttributes) {
 
         storageService.store(file);
         redirectAttributes.addFlashAttribute("message",
